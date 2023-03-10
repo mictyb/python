96d11c43a2b297f37679c47e3f06b86f11f28e77
Michał Tyburczy <122165250+mictyb@users.noreply.github.com>
2023-03-10 06:38:29 +0100

    Create BbcAverage.py
    Napisz dla BBC program sprawdzający złożoność artykułów i wpisów, dzięki czemu pracę dziennikarzy będzie można sparametryzować i automatycznie ustalić, czy piszą teksty proste i łatwe w zrozumieniu. Policz jaka jest średnia długość słów i wyświetl rezultat.

---
+++ BbcAverage.py
@@ -0,0 +1,9 @@
+ext = input(" podaj dowolny tekst Redaktorze: ")
+Words = len(Text.split())
+LengthOfChar = len(Text)
+Average = (LengthOfChar - Words)/ Words 
+
+print('liczba znakow :  ', LengthOfChar)
+print('liczba znakow :  ', Words)
+print('srednia dlugosc slow wynosi :  ', Average )
+
