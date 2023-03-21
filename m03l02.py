e3975eee23fa55bdc2dc9ca2f1b8d93cf29395a6
Michał Tyburczy <122165250+mictyb@users.noreply.github.com>
2023-03-21 07:00:39 +0100

    Create m03l02.py
    escape backslash

---
+++ m03l02.py
@@ -0,0 +1,39 @@
+
+# Stwórz i wydrukuj string, który zawiera:
+# 1. cudzysłów " (np. tekst w "cudzysłowie"), a także
+# 2. apostrof ' (np. I'm happy).
+# W jaki sposób umieścisz te znaki?
+
+# cudzyslow
+text = " text w \"cudzyslowie \" "
+
+
+print(text)
+
+
+#  apostrof
+text = " I'm happy"
+
+print(text)
+
+
+# cudzyslow i apostrof razem
+
+text = " i'm happy \" cudzyslow \" "
+print(text)
+
+
+# sciezka dostpeu
+convenient_path = r"/Users/michaltyburczy/Documents/python/Praktyczny_Python_materialy-2022-08-23/M03/M03L03_odczyt_pliku.py"
+print(convenient_path)
+print("\n")
+
+
+# w unixie ze sciezkami jest spoko gorzej w windows
+path = "/User/michal"
+print(path)
+
+
+# windows
+path = "C:\windows\\\mictyb"
+print(path)
